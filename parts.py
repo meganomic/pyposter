7a24b5e2ffc4d450bcd06dc58e8d98b76c613a8b
Anders Ponga <andersponga@gmail.com>
2015-01-10 21:29:11 +0100

    Able to post now. But it's broken somehow. Will investigate.

--- parts.py
+++ parts.py
@@ -19,15 +19,15 @@ class part:
 
 	def header(self):
 		if self.totalparts > 1:
-			return bytes('=ybegin part=' + str(self.partnr) + ' total=' + str(self.totalparts) + ' line=128 size=' + str(self.size) + ' name=' + self.name + '\n' +	'=ypart begin=' + str(self.startpos) + ' end=' + str(self.endpos) + '\n', 'utf-8')
+			return bytes('=ybegin part=' + str(self.partnr) + ' total=' + str(self.totalparts) + ' line=128 size=' + str(self.size) + ' name=' + self.name + '\r\n' +	'=ypart begin=' + str(self.startpos) + ' end=' + str(self.endpos) + '\r\n', 'utf-8')
 		else:
-			return bytes('=ybegin line=128 size=' + str(self.size) + ' name=' + name + '\n', 'utf-8')
+			return bytes('=ybegin line=128 size=' + str(self.size) + ' name=' + name + '\r\n', 'utf-8')
 
 	def trailer(self):
 		if self.totalparts > 1:
-			return bytes('\n=yend size=' + str(self.size) + ' part=' + str(self.partnr), 'utf-8')
+			return bytes('\r\n=yend size=' + str(self.size) + ' part=' + str(self.partnr), 'utf-8')
 		else:
-			return bytes('\n=yend size=' + str(self.size), 'utf-8')
+			return bytes('\r\n=yend size=' + str(self.size), 'utf-8')
 
 	def encode(self):
 		return yenc.encode(self.data)
--- pyposter.py
+++ pyposter.py
@@ -1,27 +1,53 @@
 ﻿import fnmatch, os, sys, configparser, argparse
-import yenc, parts, usenet
+import parts
+import nntplib
 
-def loadfile(filename):
+class usenet:
+	def __init__(self, serveraddress, port, username, password, head_from, head_newsgroups):
+		self.port = port
+		self.serveraddress = serveraddress
+		self.username = username
+		self.password = password
+		self.head_from = head_from
+		self.head_newsgroups = head_newsgroups
+
+	def connect(self):
+		self.server = nntplib.NNTP(self.serveraddress, self.port, self.username, self.password)
+		#self.server.set_debuglevel(1)
+
+	def quit(self):
+		self.server.quit()
+
+	def post(self, message):
+		self.server.post(message)
+
+	def message_header(self, subject):
+		return bytes('From: ' +  self.head_from + '\r\nNewsgroups: ' + self.head_newsgroups + '\r\nSubject: ' + subject + '\r\n\r\n', 'utf-8')
+
+def upload_file(filename, usenet_con):
 	with open(filename, 'rb') as f:
 		f.seek(0, 2) # Seek to EOF
-		size = f.tell() # Size of file
-		if size <= 640000: # Calculate number of segments
+		filesize = f.tell() # Size of file
+		if filesize <= 640000: # Calculate number of segments
 			segments = 1
 		else:
-			segments = int(size / 640000)
+			segments = int(filesize / 640000)
 
 		f.seek(0,0) # Reset file pointer
 		data = bytearray(640000)
 
-		for num in range(0,segments):
+		for seg in range(0,segments):
 			bytesread = f.readinto(data) # Read data
-			part = parts.part(data, num+1, segments, filename, bytesread) # Add relevant data to message part
-			message = part.header() + part.encode() + part.trailer() # Construct message
-			usenet.post(message) # Post message to usenet
+			part = parts.part(data, seg+1, segments, filename, bytesread) # Add relevant data to message part
+			if segments == 1:
+				subject = '\"' + filename + '\" ' + str(filesize) + ' yEnc bytes'
+			else:
+				subject = 'rosa psykel' + ' - \"' + filename + '\" ' + 'yEnc ' + '(' + str(seg+1) + '/' + str(segments) + ')'
+			message = usenet_con.message_header(subject) + part.header() + part.encode() + part.trailer() # Construct message
+			usenet_con.post(message) # Post message to usenet
 
 def main():
 	parser = argparse.ArgumentParser(usage='%(prog)s [options] subject newsgroup file(s)', description='Post articles to usenet.')
-	parser.add_argument('subject', help = 'Subject line to use when posting')
 	parser.add_argument('newsgroup', help = 'Newsgroup to post to')
 	parser.add_argument('files', metavar = 'file(s)', nargs = '+',
 						help = 'list of files to upload')
@@ -33,19 +59,24 @@ def main():
 	config = configparser.ConfigParser()
 	config.read('pyposter.ini')
 
-	config['pyposter']['server']
-	config['pyposter']['port']
-	config['pyposter']['from']
-	if not args.username: # Override ini file if commandline set
+	if args.username == None: # Override ini file if commandline set
 		username = config['pyposter']['username']
-	if not args.password: # Override ini file if commandline set
+	else:
+		username = args.username
+
+	if args.password == None: # Override ini file if commandline set
 		password = config['pyposter']['password']
+	else:
+		password = args.password
+
+	# Connect to usenet server
+	usenet_con = usenet(config['pyposter']['server'], config['pyposter']['port'], username, password, config['pyposter']['from'], args.newsgroup)
+	usenet_con.connect()
+
+	for file in args.files:
+		upload_file(file, usenet_con)
 	
-	print(args.files)
-	#serveraddress, port, username, password, head_from, head_newsgroups, head_subject):
-	
-	#usenet_con = usenet()
+	usenet_con.quit()
 
 if __name__ == '__main__':
 	main()
-	#loadfile('ragnarok.001')
