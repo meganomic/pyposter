9b3d88806ccf0bcbb5aa74c08f5d4fd4c48b64b9
Anders Ponga <andersponga@gmail.com>
2015-03-13 14:02:36 +0100

    Fixed nzb support when uploading multiple files. Should work now.

--- nzb.py
+++ nzb.py
@@ -2,7 +2,7 @@
 	Ultra simple mega ez nzb creator
 '''
 
-import time, random
+import time, random, html
 
 def base36(number): # Stole this function from https://github.com/tonyseek/python-base36/blob/master/base36.py
 	'''
@@ -35,37 +35,48 @@ def base36(number): # Stole this function from https://github.com/tonyseek/pytho
 
 	return value
 
-class nzb:
+class nzb_file:
 	def __init__(self, poster, subject):
-		self.poster = poster
-		self.subject = subject
-		self.groups = []
+		self.poster = html.escape(poster, True)
+		self.subject = html.escape(subject, True)
+		self.time = str(int(time.time()))
 		self.segments = []
-
-	def add_group(self, group):
-		self.groups.append(group) # The groups that the file is posted to
-
+	
 	def add_segment(self, size, partnr): # The segments of the file that was posted
 		# Make sure id is fucking unique, using 4x random numbers + current time. converting it to base36 for the lulz
 		messageid = base36(int(random.SystemRandom().randint(10000000, 99999999))) + base36(int(random.SystemRandom().randint(10000000, 99999999))) + base36(int(random.SystemRandom().randint(10000000, 99999999))) + base36(int(time.time()*1000000)) + base36(int(random.SystemRandom().randint(10000000, 99999999))) + '@' + self.poster.split(' ')[0].split('@')[1] # Create a message id using poster email and current posix time
 		self.segments.append((str(size), str(partnr), messageid))
 		return messageid # Return the Message-ID for use when posting
+	
+class nzb:
+	def __init__(self, poster, subject):
+		self.groups = []
+		self.files = []
+
+	def add_file(self, poster, subject):
+		self.files.append(nzb_file(poster, subject)) # Add a nzb_file object to the files list
+		return self.files[-1] # Return the last object in the list
+
+	def add_group(self, group):
+		self.groups.append(group) # The groups that the file is posted to
 
 	def save(self, filename): # Save nzb file to disk
 		with open(filename, 'wt') as f:
 			f.write('<?xml version=\"1.0\" encoding=\"iso-8859-1\" ?>\n')
 			f.write('<!DOCTYPE nzb PUBLIC \"-//newzBin//DTD NZB 1.1//EN\" \"http://www.newzbin.com/DTD/nzb/nzb-1.1.dtd\">\n')
 			f.write('<nzb xmlns=\"http://www.newzbin.com/DTD/2003/nzb\">\n')
-			f.write('    <file poster=\"' + self.poster + '\" date=\"' + str(int(time.time())) + '\" subject=\"' + self.subject + '\">\n')
 
-			f.write('        <groups>\n')
-			for group in self.groups: 
-				f.write('            <group>' + group + '</group>\n')
-			f.write('        </groups>\n')
+			for file in self.files:
+				f.write('    <file poster=\"' + file.poster + '\" date=\"' + str(int(time.time())) + '\" subject=\"' + file.subject + '\">\n')
 
-			f.write('        <segments>\n')
-			for size, partnr, messageid in self.segments:
-				f.write('            <segment bytes=\"' + size + '\" number=\"' + partnr + '\">' + messageid + '</segment>\n') 
-			f.write('        </segments>\n')
-			f.write('    </file>\n')
+				f.write('        <groups>\n')
+				for group in self.groups: 
+					f.write('            <group>' + group + '</group>\n')
+				f.write('        </groups>\n')
+
+				f.write('        <segments>\n')
+				for size, partnr, messageid in file.segments:
+					f.write('            <segment bytes=\"' + size + '\" number=\"' + partnr + '\">' + messageid + '</segment>\n') 
+				f.write('        </segments>\n')
+				f.write('    </file>\n')
 			f.write('</nzb>\n')
--- pyposter.py
+++ pyposter.py
@@ -22,22 +22,22 @@ class usenet:
 	def message_header(self, subject, messageid): # Format the article header
 		return bytes('From: ' +  self.head_from + '\r\nNewsgroups: ' + self.head_newsgroups + '\r\nSubject: ' + subject + '\r\nMessage-ID: <' + messageid + '>\r\n\r\n', 'utf-8')
 
-def upload_file(filename, subject, usenet_con):
+def upload_file(filename, subject, usenet_con, nzbs):
 	crc32 = zlib.crc32(open(filename,'rb').read()) & 0xffffffff # Get crc32 of entire file for yenc specification
 
-	nzb_file = nzb.nzb(usenet_con.head_from, subject)
-	nzb_file.add_group(usenet_con.head_newsgroups)
 	with open(filename, 'rb') as f:
 		f.seek(0, 2) # Seek to EOF
 		filesize = f.tell() # Size of file
 		if filesize <= 640000: # Calculate number of segments
 			segments = 1 # If size is below 640000 then there is only 1 segment.
+			nzfile = nzbs.add_file(usenet_con.head_from, subject + ' - \"' + filename + '\" ' + str(filesize) + ' yEnc bytes')
 		else:
 			segments = int(filesize / 640000)
+			nzfile = nzbs.add_file(usenet_con.head_from, subject + ' - \"' + filename + '\" ' + 'yEnc ' + '(1' + '/' + str(segments) + ')')
 
 		f.seek(0,0) # Reset file pointer
 		data = bytearray(640000) # Need a buffer for reading the file into
-
+		
 		for seg in range(0,segments): # Let's go through the segments!
 			bytesread = f.readinto(data) # Read data
 			part = parts.part(data, seg+1, segments, filename, bytesread, crc32) # Add relevant data to message part
@@ -45,14 +45,14 @@ def upload_file(filename, subject, usenet_con):
 				subject_ed = subject + ' - \"' + filename + '\" ' + str(filesize) + ' yEnc bytes'
 			else:
 				subject_ed = subject + ' - \"' + filename + '\" ' + 'yEnc ' + '(' + str(seg+1) + '/' + str(segments) + ')'
-			messageid = nzb_file.add_segment(bytesread, seg+1)
+			messageid = nzfile.add_segment(bytesread, seg+1)
 			article = usenet_con.message_header(subject_ed, messageid) + part.header() + part.encode() + part.trailer() # Construct message
-			usenet_con.post(article) # Post article to usenet
-	nzb_file.save(filename.split('.')[0] + '.nzb')
+			#usenet_con.post(article) # Post article to usenet
+			
 
 def main():
 	# Need some commandline options
-	parser = argparse.ArgumentParser(usage='%(prog)s [options] subject newsgroup file(s)', description='Post articles to usenet.')
+	parser = argparse.ArgumentParser(usage='%(prog)s [options] subject newsgroup file(s)', description='Post files to usenet.')
 	parser.add_argument('subject', help = 'Subject line')
 	parser.add_argument('newsgroup', help = 'Newsgroup to post to')
 	parser.add_argument('files', metavar = 'file(s)', nargs = '+',
@@ -77,13 +77,18 @@ def main():
 
 	# Setup usenet connection
 	usenet_con = usenet(config['pyposter']['server'], config['pyposter']['port'], username, password, config['pyposter']['from'], args.newsgroup)
-	usenet_con.connect() # Connect to server
+	#usenet_con.connect() # Connect to server
+
+	nzbs = nzb.nzb(config['pyposter']['from'], args.subject)
+	nzbs.add_group(args.newsgroup)
 
 	for filearg in args.files: # Iterate through file list
 		for file in glob.glob(filearg): # Expand possible wildcards and iterate over results
-			upload_file(file, args.subject, usenet_con) # Go upload the files!
+			upload_file(file, args.subject, usenet_con, nzbs) # Go upload the files!
 
-	usenet_con.quit() # Remember to disconnect =)
+	#usenet_con.quit() # Remember to disconnect =)
+
+	nzbs.save(args.subject + '.nzb')
 
 if __name__ == '__main__':
 	main()
