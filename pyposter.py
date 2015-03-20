0ed010a67b43240c504cec9d3fc7d50e7cdd6865
Anders Ponga <andersponga@gmail.com>
2015-03-20 11:49:37 +0100

    Prettier code

--- pyposter.py
+++ pyposter.py
@@ -16,9 +16,9 @@ class usenetfile():
 	def __init__(self, filename, subject):
 		self.filename = filename
 		self.subject = subject
-		self.currentsegment = -1
+		self.currentsegment = 0
 		self.crc32 = None # crc32 of the original file
-		self.totalsegments = None # To
+		self.totalsegments = None
 		self.filesize = None
 		self.fd = open(filename, 'rb') # I like to live on the edge
 
@@ -45,18 +45,21 @@ class usenetfile():
 		segmentsize = self.fd.readinto(data)
 		if segmentsize	== 0: # Check if we are on the last segment
 			raise StopIteration
-		self.currentsegment = self.currentsegment + 1
+
 		pcrc32 = zlib.crc32(data) & 0xffffffff # crc32 of this part
 		startpos = self.currentsegment * self.blocksize # Save the positions in the original file for this part so we can put it back together
 		endpos = startpos + segmentsize
-		
+
+		# Yes this is correct
+		self.currentsegment = self.currentsegment + 1 # Advance the segment counter
+
 		# Make sure id is fucking unique, using 4x random numbers + current time. converting it to base36 for the lulz
 		messageid = self.__base36(int(random.SystemRandom().randint(10000000, 99999999))) + self.__base36(int(random.SystemRandom().randint(10000000, 99999999))) + self.__base36(int(random.SystemRandom().randint(10000000, 99999999))) + self.__base36(int(time.time()*1000000)) + self.__base36(int(random.SystemRandom().randint(10000000, 99999999))) + '@' + self.poster.split(' ')[0].split('@')[1] # Create a message id using poster email and current posix time and some random numbers
-		self.nzbfile.addsegment(segmentsize, self.currentsegment + 1, messageid)
+		self.nzbfile.addsegment(segmentsize, self.currentsegment, messageid)
 		
 		if self.totalsegments > 1: # The yenc specification differs for multipart files vs singlepart files
-			subject = self.subject + ' - \"' + self.filename + '\" ' + 'yEnc ' + '(' + str(self.currentsegment + 1) + '/' + str(self.totalsegments) + ')'
-			yencheader = bytes('=ybegin part=' + str(self.currentsegment + 1) + ' total=' + str(self.totalsegments) + ' line=128 size=' + str(segmentsize) + ' name=' + self.filename + '\r\n' +	'=ypart begin=' + str(startpos) + ' end=' + str(endpos) + '\r\n', 'utf-8')
+			subject = self.subject + ' - \"' + self.filename + '\" ' + 'yEnc ' + '(' + str(self.currentsegment) + '/' + str(self.totalsegments) + ')'
+			yencheader = bytes('=ybegin part=' + str(self.currentsegment) + ' total=' + str(self.totalsegments) + ' line=128 size=' + str(segmentsize) + ' name=' + self.filename + '\r\n' +	'=ypart begin=' + str(startpos) + ' end=' + str(endpos) + '\r\n', 'utf-8')
 			yenctrailer = bytes('\r\n=yend size=' + str(segmentsize) + ' part=' + str(self.currentsegment) + ' pcrc32=' + str(hex(pcrc32))[2:10] + ' crc32=' + str(hex(self.crc32))[2:10], 'utf-8')
 		else:
 			subject = self.subject + ' - \"' + self.filename + '\" ' + str(self.filesize) + ' yEnc bytes'
@@ -66,12 +69,14 @@ class usenetfile():
 		# Usenet article header
 		usenetheader = bytes('poster: ' +  self.poster + '\r\nNewsgroups: ' + ','.join(self.newsgroups).rstrip(',') + '\r\nSubject: ' + self.subject + '\r\nMessage-ID: <' + messageid + '>\r\n\r\n', 'utf-8')
 
+		# Encode the data using yenc specification
 		self.cyenc.argtypes = [ctypes.POINTER(ctypes.c_char), ctypes.POINTER(ctypes.c_char), ctypes.c_int] # Convert to these types when calling dll function pls
 		encodedoutput = ctypes.create_string_buffer(self.blocksize*2) # Create a big buffer so we don't run out of space
 		encoded_size = self.cyenc.encode(ctypes.create_string_buffer(bytes(data)), encodedoutput, segmentsize) # Encode to yenc using external dll
 
 		article = usenetheader + yencheader + encodedoutput[0:encoded_size] + yenctrailer # Construct message
-		return article
+
+		return article # Return fully formed usenet article
 
 	def __base36(self, number): # Stole this function poster https://github.com/tonyseek/python-base36/blob/master/base36.py
 		alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
