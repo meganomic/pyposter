7114983331f1299260601d3bb85673a1ff82f691
Anders Ponga <andersponga@gmail.com>
2015-03-28 20:22:38 +0100

    It seems to work correctly.

--- cyencmmx.asm
+++ cyencmmx.asm
@@ -6,84 +6,89 @@ section .code bits 64
 encode:
 	push r12 ; save r12 and r13, I need those registers for stuff
 	push r13
+	push rbx
+	xor rbx, rbx
 	xor r9, r9
 	xor r11, r11
 	xor r13, r13
+	
+	movq mm6, [special4]
+	movq mm5, [special3]
+	movq mm4, [special2]
+	movq mm3, [special1]
 
 .encodeset:
 	cmp r8, 8
-	jle .lasteight ; The last 8 or less characters need special treatment	
+	jle .lasteight ; The last 8 or less characters need special treatment
 	movq mm0, [rcx] ; Move character from memory to register
-	mov r10, const1
-	movq mm1, [r10]
-	paddb mm0, mm1
-	mov r12, const2
-	movq mm1, [r12]
-	pand mm0, mm1
-	jmp .checkforspecial
+	paddb mm0, [const1] ; + 42
+	;pand mm0, [const2] ; modulus 256
 
-.checkforspecial:
 	movq mm7, mm0 ; temporary copy
-	mov r10, special1
-	pcmpeqb mm7, [r10]
+	pcmpeqb mm7, mm3
 	movq r10, mm7
 	cmp r10, 0
-	jne .specialchar
+	jne .scmultientry
 	movq mm7, mm0
-	mov r10, special2
-	pcmpeqb mm7, [r10]
+	pcmpeqb mm7, mm4
 	movq r10, mm7
 	cmp r10, 0
-	jne .specialchar
+	jne .scmultientry
 	movq mm7, mm0
-	mov r10, special3
-	pcmpeqb mm7, [r10]
+	pcmpeqb mm7, mm5
 	movq r10, mm7
 	cmp r10, 0
-	jne .specialchar
+	jne .scmultientry
 	movq mm7, mm0
-	mov r10, special4
-	pcmpeqb mm7, [r10]
+	pcmpeqb mm7, mm6
 	movq r10, mm7
 	cmp r10, 0
-	jne .specialchar
+	jne .scmultientry
 	jmp .writesettobuffer
 
+.scmultientry:
+	movq rax, mm0
+	
 .scmulti:
 	add r13, 1
-	movq rax, mm0
-	;mov r10b, al ; Move character from memory to register
-	cmp al, 0 ; Check for illegal characters
+	mov bl, al
+	cmp bl, 0 ; Check for illegal characters
 	je .scmulti2
-	cmp al, 10
+	cmp bl, 10
 	je .scmulti2
-	cmp al, 13
+	cmp bl, 13
 	je .scmulti2
-	cmp al, 61
+	cmp bl, 61
 	je .scmulti2
+	jmp .scnextcharmulti
 
 .scmulti2:
-	add al, 64 ; This time we add 64
-	and al, 255 ; ultra fast modulus!! again
+	add bl, 64 ; This time we add 64
+	and bl, 255 ; ultra fast modulus!! again
 	mov byte [rdx], 61 ; Add escape character
 	add rdx, 1 ; increase output array pointer
 	add r9, 1 ; Increase size of output
 	add r11, 1 ; Increase line length
-	mov byte [rdx], al ; Move encoded byte to output array
-	add rdx, 1 ; increase output array pointer
-	add r9, 1 ; Increase size of output
-	add r11, 1 ; Increase line length
-	cmp r11, 127
-	jge .scnewlinemulti
-	cmp r13, 7
-	je .nextset
 	jmp .scnextcharmulti
 
 .scnextcharmulti:
-	rol rax, 8
+	mov byte [rdx], bl ; Move encoded byte to output array
+	add rdx, 1 ; increase output array pointer
+	add r9, 1 ; Increase size of output
+	add r11, 1 ; Increase line length
+	ror rax, 8
 	sub r8, 1
-	jnz .scmulti
-	jmp .exitprogram
+	jz .exitprogram
+	cmp r11, 127
+	jge .scnewlinemulti
+	cmp r13, 8
+	je .nextset
+	jmp .scmulti
+
+.nextset:
+	xor r13, r13
+	add rcx, 8
+	jmp .encodeset
 
 .scnewlinemulti:
 	mov byte [rdx], 13 ; \r
@@ -95,12 +100,11 @@ encode:
 	xor r11, r11
 	cmp r13, 8
 	je .nextset
-	jmp .scnextcharmulti
+	jmp .scmulti
 
 .specialchar:
 	add r13, 1
 	mov r10b, byte [rcx] ; Move character from memory to register
-	rol rax, 8
 	add r10b, 42 ; Add 42 before modulus
 	and r10b, 255 ; ultra fast modulus!!
 	cmp r10b, 0 ; Check for illegal characters
@@ -167,13 +171,6 @@ encode:
 .lasteight:
 	jmp .specialchar
 
-.nextset:
-	xor r13, r13
-	add rcx, 1
-	sub r8, 1
-	jnz .encodeset
-	jmp .exitprogram
-
 .newline:
 	mov byte [rdx], 13 ; \r
 	add rdx, 1 ; increase output array pointer
@@ -187,14 +184,15 @@ encode:
 .exitprogram:
 	mov rax, r9 ; Return output size
 	emms
+	pop rbx
 	pop r13 ; restore some registers to their original state
 	pop r12
 	ret
 
 section .data
 special1:	dq 0x3D0D0A003D0D0A00
-special2:	dq 0x003D0D0A003D0D0A
+special2:	dq 0x0D0A003D0D0A003D
 special3:	dq 0x0A003D0D0A003D0D
-special4:	dq 0x0D0A003D0D0A003D
+special4:	dq 0x003D0D0A003D0D0A
 const1:		dq 0x2A2A2A2A2A2A2A2A
 const2:		dq 0xFFFFFFFFFFFFFFFF
